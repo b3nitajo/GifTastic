f3701526e49de1d5cfbb0220db7aa948eac1e145
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-03-30 23:24:12 -0500

    add button code

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -39,7 +39,56 @@ $("button").on("click", function() {
           // Prependng the favFoodDiv to the HTML page in the "#gifs-appear-here" div
           $("#gifs-appear-here").prepend(favFoodDiv);
         }
+    });
+ });
+
+ 
+ //build user search query
+function buildQueryURL() {
+    var queryParams = $("#userFavFood").val().trim();
+    
+    // Grab text the user typed into the search input, add to the queryParams object
+    
+
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jGgqEkH19SV9vhks3KcMO0PvjHILSOnS&q=" +
+    queryParams + "&limit=10&offset=0&rating=G&lang=en";
+  
+    // Logging the URL so we have access to it for troubleshooting
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
+    console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+}
+
+$("#userSearch").on("click", function(event) {
+    // This line allows us to take advantage of the HTML "submit" property
+    // This way we can hit enter on the keyboard and it registers the search
+    // (in addition to clicks). Prevents the page from reloading on form submit.
+    event.preventDefault();
+
+    creatButton();
+  
+    // Empty the region associated with the articles
+   // clear();
+  
+    // Build the query URL for the ajax request to the NYT API
+    var queryURL = buildQueryURL();
+  
+    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+    // The data then gets passed as an argument to the updatePage function
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+    });
+});
 
 
-      });
-  });
+function creatButton() {
+    var queryParams = $("#userFavFood").val().trim();
+    var newButton = $('<button>');
+    newButton.attr("data-favFood", queryParams);
+    newButton.text(queryParams);
+    $("#foodButtons").append(newButton);
+}
