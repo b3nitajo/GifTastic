203949b077e173a6bc62f1bff57cf1d9523dcf74
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-04-01 00:50:21 -0500

    final submission working giftastic

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,92 +1,88 @@
-// Adding click event listen listener to all buttons
-$(".favFoods").on("click", function() {
-    // Grabbing and storing the data-favFood property value from the button
+//VARIABLES
+var topics = ["pizza", "chicken", "fries"];
+var userfavFoodArr = [];
+createButton(topics);
+
+// ADD LISTENER TO FAV FOOD CLICK EVENT
+$(document).on("click", ".favFoods", function () {
     var favFood = $(this).attr("data-favFood");
 
-    // Constructing a queryURL using the favFood name
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jGgqEkH19SV9vhks3KcMO0PvjHILSOnS&q=" +
     favFood + "&limit=10&offset=0&rating=G&lang=en";
 
-    // Performing an AJAX request with the queryURL
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // After data comes back from the request
+    
       .then(function(response) {
         console.log(queryURL);
 
         console.log(response);
-        // storing the data from the AJAX request in the results variable
         var results = response.data;
 
     //ADD GIPHY TO IMAGE LIST AFTER BUTTON CLICK
-        // Looping through each result item
         for (var i = 0; i < results.length; i++) {
-          // Creating and storing a div tag
+        //creating div for images
           var favFoodDiv = $("<div>");
-          favFoodDiv.attr("class", "foodImages");
-          // Creating a paragraph tag with the result item's rating
+          favFoodDiv.addClass("foodImages");
           var p = $("<p>").text("Rating: " + results[i].rating);
-          // Creating and storing an image tag
-          var favFoodImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          favFoodImage.attr("src", results[i].images.fixed_height.url);
-          // Appending the paragraph and image tag to the favFoodDiv
-          //var favFoodCol = $(".col");
           favFoodDiv.append(p);
+        //animation url variables
+          var still = results[i].images.fixed_height_still.url;
+          var animate = results[i].images.fixed_height.url;
+        //creating images
+          var favFoodImage = $('<img>');
+          favFoodImage.attr("src", animate);
+          favFoodImage.attr('data-animate', animate);
+          favFoodImage.attr('data-still', still);
+          favFoodImage.attr('data-state', 'animate');
+        //add images to div and add on click listener
           favFoodDiv.append(favFoodImage);
-          // Prependng the favFoodDiv to the HTML page in the "#gifs-appear-here" div
           $("#gifs-appear-here").prepend(favFoodDiv);
+          favFoodImage.on('click', animateGif);
         }
     });
- });
 
- 
- //build user search query
-function buildQueryURL() {
-    var queryParams = $("#userFavFood").val().trim();
-    
-    // Grab text the user typed into the search input, add to the queryParams object
-    
-
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=jGgqEkH19SV9vhks3KcMO0PvjHILSOnS&q=" +
-    queryParams + "&limit=10&offset=0&rating=G&lang=en";
-  
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
-}
-
-$("#userSearch").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-
-  
-    // Empty the region associated with the articles
-   // clear();
-  
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(creatButton); 
-  //  });
+    //GIF ANIMATION
+    function animateGif () {
+        var foodGifState = $(this).attr('data-state');    
+        if (foodGifState === 'still') {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } 
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+    }
 });
 
-function creatButton() {
-    var queryParams = $("#userFavFood").val().trim();
-    var newButton = $('<button>');
-    newButton.attr("data-favFood", queryParams);
-    newButton.attr("class", "favFoods");
-    newButton.text(queryParams);
-    $("#foodButtons").append(newButton);
-}
+
+ //CREATE BUTTONS WHEN USER ENTERS A NEW FAV FOOD
+$("#userSearch").on("click", function(event) {
+    event.preventDefault();
+    var userFavFood = $('#userFavFood').val().trim();
+    topics.push(userFavFood);
+    var userButtons = createButton(topics);
+    $("#foodButtons").append(userButtons);
+    console.log(userfavFoodArr);
+    $('#userFavFood').val('');
+});
+
+//CREATE BUTTONS ON PAGE
+function createButton(arr){
+    $("#foodButtons").empty();
+    for (var i=0; i < arr.length; i++){
+        var newButton = $('<button>');
+        newButton.attr("data-favFood", arr[i]);
+        console.log("data-favFood", arr[i]);
+        newButton.addClass("favFoods");
+        newButton.text(arr[i]);
+        $("#foodButtons").append(newButton);
+    }    
+ };
+
+
+
+
